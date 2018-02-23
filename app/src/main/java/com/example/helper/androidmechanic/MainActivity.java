1cd6c7f3d341ed9e1bbabd108444206a76f2ebcb
abhas1102 <abhas1102@gmail.com>
2018-02-23 15:49:14 +0530

    Alert Dialogs have been added while registering.

--- app/src/main/java/com/example/helper/androidmechanic/MainActivity.java
+++ app/src/main/java/com/example/helper/androidmechanic/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.helper.androidmechanic;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
@@ -12,6 +18,10 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignIn , btnRegister;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -26,11 +36,33 @@ public class MainActivity extends AppCompatActivity {
                 setDefaultFontPath("fonts/Arkhip_font.ttf").setFontAttrId(R.attr.fontPath).build());
         setContentView(R.layout.activity_main);
 
+        //init firebase
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
         //init view
 
         btnRegister = (Button)findViewById(R.id.btnRegister);
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
 
+        //Events
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterDialog();
+            }
+        });
+
+
+    }
+
+    private void showRegisterDialog() {
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("REGISTER");
+        dialog.setMessage("Please use Email to register");
     }
 }
